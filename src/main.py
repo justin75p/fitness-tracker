3333d61500b5fb4f2209103922f7c7091cef6e84
Justin <justin75p@gmail.com>
2025-07-10 20:41:14 -0700

    wrapped all inputs for a new user around a form element

--- src/main.py
+++ src/main.py
@@ -11,20 +11,21 @@ database = Database()
 st.title("Fitness Tracker")
 
 user_selection = st.selectbox("Select a user:", database.get_all_user_names(), index= 0)
-new_user_name = st.text_input("Create a new user:", placeholder= "New user's name")
 
-if new_user_name:
+with st.form("user_creation_form", clear_on_submit= True):
+    new_user_name = st.text_input("Create a new user:", placeholder= "New user's name")
+
     starting_weight = st.text_input("Enter your starting weight (lbs):", placeholder= "New user's starting weight")
+
+    goal_weight = st.text_input("Enter your goal weight (lbs):", placeholder= "New user's goal weight")
+
+    maintenance_calories = st.text_input("Enter your maintenance calories:", placeholder= "New user's maintenance calories")
     
-    if starting_weight:
-        goal_weight = st.text_input("Enter your goal weight (lbs):", placeholder= "New user's goal weight")
+    submit = st.form_submit_button("Create user")
 
-        if goal_weight:
-            maintenance_calories = st.text_input("Enter your maintenance calories:", placeholder= "New user's maintenance calories")
-
-            if maintenance_calories:
-                user_creation_button = st.button("Create User")
-
-                if user_creation_button:
-                    database.insert_user(User(new_user_name, starting_weight, goal_weight, maintenance_calories))
-                    # TODO: get button to refresh site page
+    if submit:
+        if new_user_name and starting_weight and goal_weight and maintenance_calories:
+            database.insert_user(User(new_user_name, starting_weight, goal_weight, maintenance_calories)) 
+            st.success(f"""User '{new_user_name}' created!""")
+        else:
+            st.warning("Please fill in all required fields!")
