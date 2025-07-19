62eb0dd6d443b04518696c60b649c137bf889cab
Justin <justin75p@gmail.com>
2025-07-19 15:11:07 -0700

    update test_trackables with new water field

--- test/test_trackables.py
+++ test/test_trackables.py
@@ -10,8 +10,8 @@ user = User("Justin", 165, 155, 2700)
 print(f"Name: {user.name}, Starting Weight: {user.starting_weight}, Goal Weight: {user.goal_weight}, Maintenance Calories: {user.maintenance_calories}")
 
 # Test DailyEntry class
-entry = DailyEntry(date.today(), 165, 2200, 10000, 8)
-print(f"Daily Entry - Date: {entry.entry_date}, Weight: {entry.weight}, Calories: {entry.calories} Steps: {entry.steps}, Sleep: {entry.sleep} hours")
+entry = DailyEntry(date.today(), 165, 2200, 3000, 10000, 8)
+print(f"Daily Entry - Date: {entry.entry_date}, Weight: {entry.weight}, Calories: {entry.calories}, Water: {entry.water}, Steps: {entry.steps}, Sleep: {entry.sleep} hours")
 
 # Test WorkoutEntry class
 workout = WorkoutEntry(date.today(), time(16, 30), "Running", 30, "Moderate")
